2b8fabe940acc1f3388590d6febd2b5a88cafea2
김중우 <tirkffus@naver.com>
2021-11-02 14:58:19 +0900

    project

--- src/main/java/com/ssafy/happyhouse/HappyHouseSpringApplication.java
+++ src/main/java/com/ssafy/happyhouse/HappyHouseSpringApplication.java
@@ -1,9 +1,13 @@
 package com.ssafy.happyhouse;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
+@MapperScan(basePackages = "com.ssafy.happyhouse.model.repo")
 public class HappyHouseSpringApplication {
 
 	public static void main(String[] args) {
